aeb19663bdee30a29ee8c1e1ef8ced6789b97c03
kokheng1025 <kokhenglai1025@gmail.com>
2018-09-16 13:13:42 +0800

    minor change for using Google Cloud GPU VM

--- dogs_vs_cats/config/dogs_vs_cats_config.py
+++ dogs_vs_cats/config/dogs_vs_cats_config.py
@@ -1,5 +1,5 @@
 # define the paths to the images directory
-IMAGES_PATH = ".\\datasets\\kaggle_dogs_vs_cats\\train"
+IMAGES_PATH = "./datasets/kaggle_dogs_vs_cats/train"
 
 # since we do not have validation data or access to the testing
 # lables we need to take a number of images from the training
@@ -10,16 +10,16 @@ NUM_TEST_IMAGES = 1250 * NUM_CLASSES
 
 # define the path to the output training, validation, and testing 
 # HDF5 files
-TRAIN_HDF5 = ".\\datasets\\kaggle_dogs_vs_cats\\hdf5\\train.hdf5"
-VAL_HDF5 = ".\\datasets\\kaggle_dogs_vs_cats\\hdf5\\val.hdf5"
-TEST_HDF5 = ".\\datasets\\kaggle_dogs_vs_cats\\hdf5\\test.hdf5"
+TRAIN_HDF5 = "./datasets/kaggle_dogs_vs_cats/hdf5/train.hdf5"
+VAL_HDF5 = "./datasets/kaggle_dogs_vs_cats/hdf5/val.hdf5"
+TEST_HDF5 = "./datasets/kaggle_dogs_vs_cats/hdf5/test.hdf5"
 
 # path to the output model file
-MODEL_PATH = ".\\output\\alexnet_dogs_vs_cats.model"
+MODEL_PATH = "./output/alexnet_dogs_vs_cats.model"
 
 # define the path to the dataset mean
-DATASET_MEAN = ".\\output\\dogs_vs_cats_mean.json"
+DATASET_MEAN = "./output/dogs_vs_cats_mean.json"
 
 # define the path to the output directory used for storing plots,
 # classification reports, etc.
-OUTPUT_PATH = ".\\output"
+OUTPUT_PATH = "./output"
